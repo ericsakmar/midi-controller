0813351da1cb38d8f47343817971e1d1fd1f3ff4
Eric Sakmar <esakmar@truefit.io>
2023-12-01 14:24:00 -0500

    adds switch class

--- code.py
+++ code.py
@@ -14,8 +14,6 @@ from adafruit_debouncer import Debouncer
 
 # midi stuff
 midi_out_channel = 12
-midi_knob_cc = 100
-midi_switch_cc = 101
 uart = busio.UART(board.TX, board.RX, baudrate=31250, timeout=0.001)
 midi = adafruit_midi.MIDI(midi_in=uart, midi_out=uart, out_channel=midi_out_channel)
 
@@ -36,29 +34,38 @@ class Knob:
             midi.send(cc)
             self.last = value
 
+class Switch:
+    def __init__(self, input, led_pin, midi_control):
+        self.midi_control = midi_control
+
+        self.pin = digitalio.DigitalInOut(input)
+        self.pin.direction = digitalio.Direction.INPUT
+        self.pin.pull = digitalio.Pull.UP
+
+        self.switch = Debouncer(self.pin)
+        self.switch_on = False
+
+        self.led = digitalio.DigitalInOut(led_pin)
+        self.led.direction = digitalio.Direction.OUTPUT
+
+    def update(self):
+        self.switch.update()
+
+        if self.switch.fell:
+            self.switch_on = not self.switch_on
+            self.led.value = self.switch_on
+            midi_value = 127 if self.switch_on else 0
+            cc = ControlChange(self.midi_control, midi_value, channel=midi_out_channel)
+            print(cc)
+            midi.send(cc)
+
 # pot stuff
 knob1 = Knob(board.A0, 100)
 knob2 = Knob(board.A1, 101)
 
 # switch stuff
-pin = digitalio.DigitalInOut(board.D5)
-pin.direction = digitalio.Direction.INPUT
-pin.pull = digitalio.Pull.UP
-switch = Debouncer(pin)
-switch_on = False
-
-pin2 = digitalio.DigitalInOut(board.D6)
-pin2.direction = digitalio.Direction.INPUT
-pin2.pull = digitalio.Pull.UP
-switch2 = Debouncer(pin2)
-switch_2_on = False
-
-# led stuff
-led = digitalio.DigitalInOut(board.D13)
-led.direction = digitalio.Direction.OUTPUT
-
-led2 = digitalio.DigitalInOut(board.D12)
-led2.direction = digitalio.Direction.OUTPUT
+switch1 = Switch(board.D5, board.D13, 106)
+switch2 = Switch(board.D6, board.D12, 107)
 
 # Main loop
 while True:
@@ -68,24 +75,10 @@ while True:
         # print("MIDI Message received:", msg)
         midi.send(msg) # add msg.channel?
 
-    # knobs
     knob1.update()
     knob2.update()
 
-    # switches
-    switch.update()
-    if switch.fell:
-        switch_on = not switch_on
-        led.value = switch_on
-        midi_value = 127 if switch_on else 0
-        cc = ControlChange(midi_switch_cc, midi_value, channel=midi_out_channel)
-        print(cc)
-        midi.send(cc)
-
+    switch1.update()
     switch2.update()
-    if switch2.fell:
-        switch_2_on = not switch_2_on
-        led2.value = switch_2_on
-        print(switch_2_on)
 
     # time.sleep(1)
