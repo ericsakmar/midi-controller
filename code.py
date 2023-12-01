5fde78f5ce24dc8e3a97710e7933a511853ee442
Eric Sakmar <esakmar@truefit.io>
2023-12-01 13:59:33 -0500

    adds switch and knob

--- code.py
+++ code.py
@@ -24,6 +24,9 @@ midi = adafruit_midi.MIDI(midi_in=uart, midi_out=uart, out_channel=midi_out_chan
 p0 = analogio.AnalogIn(board.A0)
 p0_last = 0
 
+p1 = analogio.AnalogIn(board.A1)
+p1_last = 0
+
 # switch stuff
 pin = digitalio.DigitalInOut(board.D5)
 pin.direction = digitalio.Direction.INPUT
@@ -31,9 +34,18 @@ pin.pull = digitalio.Pull.UP
 switch = Debouncer(pin)
 switch_on = False
 
+pin2 = digitalio.DigitalInOut(board.D6)
+pin2.direction = digitalio.Direction.INPUT
+pin2.pull = digitalio.Pull.UP
+switch2 = Debouncer(pin2)
+switch_2_on = False
+
 # led stuff
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
+
+led2 = digitalio.DigitalInOut(board.D12)
+led2.direction = digitalio.Direction.OUTPUT
 
 # Main loop
 while True:
@@ -48,10 +60,17 @@ while True:
     difference = math.fabs(sensor_value - p0_last)
     if math.fabs(difference) > 258:
         midi_value = int((sensor_value / 65535) * 127)
-        print("SENDING", midi_value)
+        print("SENDING p0", midi_value)
         cc = ControlChange(midi_knob_cc, midi_value, channel=midi_out_channel)
         midi.send(cc)
         p0_last = sensor_value
+
+    sensor_1_value = p1.value
+    d1 = math.fabs(sensor_1_value - p1_last)
+    if d1 > 258:
+        midi_value = int((sensor_1_value / 65535) * 127)
+        print("SENDING p1", midi_value)
+        p1_last = sensor_1_value
 
     # switches
     switch.update()
@@ -62,5 +81,11 @@ while True:
         cc = ControlChange(midi_switch_cc, midi_value, channel=midi_out_channel)
         print(cc)
         midi.send(cc)
+
+    switch2.update()
+    if switch2.fell:
+        switch_2_on = not switch_2_on
+        led2.value = switch_2_on
+        print(switch_2_on)
 
     # time.sleep(1)
